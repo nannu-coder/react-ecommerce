31fe9ded20962d467deb1c3149783aa741ab8cac
Nazmul Hasan Nannu <nazmulnannu43@gmail.com>
2023-01-31 01:30:47 +0600

    initial commit

---
+++ src/Pages/index.js
@@ -0,0 +1,19 @@
+import About from "./About";
+import Cart from "./Cart";
+import CheckOut from "./CheckOut";
+import Error from "./Error";
+import Home from "./Home";
+import PrivateRoute from "./PrivateRoute";
+import Products from "./Products";
+import SingleProduct from "./SingleProduct";
+
+export {
+  About,
+  Cart,
+  CheckOut,
+  Error,
+  Home,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+};
